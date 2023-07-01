db1d5a789a2b7516dda2fb68270428c759b8c39d
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-07-01 15:28:43 -0700

    added links

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 import React from 'react';
 
 function App() {
+  const title = 'Welcome to my Blog';
+  const likes = 50;
+  const link = 'http://www.google.com'
   return (
     <div className="App">
-      <h1> App Component</h1>
+      <h1> {title} </h1>
+      <p>liked {likes} times</p>
+      < a href={link}> google site</a>
     </div>
   );
 }
