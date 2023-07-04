bdc79f40c397313b1a39767c57fca8847e49ac89
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-07-04 01:23:26 -0700

    added files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 
-import './App.css';
-import React from 'react';
 
+import React from 'react';
+import Navbar from './Navbar';
+import Home from './Home';
 function App() {
-  const title = 'Welcome to my Blog';
-  const likes = 50;
-  const link = 'http://www.google.com'
+
   return (
     <div className="App">
-      <h1> {title} </h1>
-      <p>liked {likes} times</p>
-      < a href={link}> google site</a>
+      <Navbar></Navbar>
+      <div className='content'>
+        <Home/>
+      </div>
     </div>
   );
 }
