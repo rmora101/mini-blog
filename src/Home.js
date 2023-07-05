4add840ff7f2609f777fc9f8c66dff0d19d25a6e
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-07-05 15:57:54 -0700

    added button

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,15 @@
 const Home = () => {
+    
+    const handleClick = () =>{
+        console.log('i been clicked on')
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
         </div>
-     );
+    );
 }
  
 export default Home;
