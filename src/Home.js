8b78482b34baa4f87ebfe4f994533dc69c6a2aeb
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-07-07 14:07:13 -0700

    lift state and hooks

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,19 @@
+import { useState } from "react";
+
 const Home = () => {
     
+    const [name, setName]= useState('msrio');
+    const [age, Setage] = useState(25);
+
     const handleClick = () =>{
-        console.log('i been clicked on')
+        setName('luigi');
+        Setage(15);
     }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} {age}</p>
             <button onClick={handleClick}>Click me</button>
         </div>
     );
