b0de423af7519d80ae9e8c241b05cf8d35d8b162
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-07-11 14:52:11 -0700

    added blog entries and styling to it

--- src/Home.js
+++ src/Home.js
@@ -2,19 +2,19 @@ import { useState } from "react";
 
 const Home = () => {
     
-    const [name, setName]= useState('msrio');
-    const [age, Setage] = useState(25);
-
-    const handleClick = () =>{
-        setName('luigi');
-        Setage(15);
-    }
+    const [blogs, setBlogs] = useState([
+        {title: 'Ruth mora site', body: 'fhfeiuw fhusifgrh ...', author: 'me', id:1},
+        {title: 'mini site', body: 'fhfeiuw fhusifgrh ...', author: 'u', id:2}
+    ])
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} {age}</p>
-            <button onClick={handleClick}>Click me</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p> written by {blog.author}</p>
+                </div>
+            ))}
         </div>
     );
 }
